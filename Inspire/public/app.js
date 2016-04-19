91c5fb3ce66b1fa67898994fb681be894d8a2d65
Aralya Phinith <aphinith84@gmail.com>
2016-04-19 12:35:21 -0700

    Server functioning and local host page loading correctly

---
+++ Inspire/public/app.js
@@ -0,0 +1,129 @@
+angular.module('inspire', [])
+.controller('generateQuotesCtrl', function($scope, $http) {
+  // $http.defaults.headers.common["X-TheySaidSo-Api-Secret"] = 9lfbA8Lt0DgYlKpNgV4hkweF;
+  $scope.quote;
+  $scope.author;
+  $scope.favorites = [];
+  var search = 'http://quotes.rest/quote.json?category=';
+  var key = '&api_key=9lfbA8Lt0DgYlKpNgV4hkweF';
+
+  //****************************************************************************************
+  //function for inspirational quotes
+  $scope.inspire = function() {
+    // $scope.quote = "Here is an inspirational quote";
+    var options = ['inspire', 'positive', 'confidence', 'self-improvement', 'happiness', 'attitude', 'motivational', 'inspirational', 'success', 'successful-mind'];
+    var ranNum = Math.floor(Math.random() * 10);
+    $http({
+      method: 'GET',
+      url: search + options[ranNum] + key
+    }).success(function (response) {
+        console.log('success');
+        // console.log(response);
+        var quote = response.contents.quote;
+        var author = response.contents.author;
+        $scope.quote = quote;
+        $scope.author = author;
+
+      },
+      function (response) {
+      console.log('error');
+    })
+  };
+
+  //function for wisdom quotes
+  $scope.wise = function() {
+    // $scope.quote = "Here is a word to the wise";
+    var options = ['wise', 'hope', 'wisdom', 'tso-life', 'doingyourbest', 'conscience', 'philosophy', 'existentialism', 'beauty', 'enlightenment'];
+    var ranNum = Math.floor(Math.random() * 10);
+    $http({
+      method: 'GET',
+      url: search + options[ranNum] + key
+    }).success(function successCallback(response) {
+      console.log('success');
+      var quote = response.contents.quote;
+      var author = response.contents.author;
+      $scope.quote = quote;
+      $scope.author = author;
+    },
+    function errorCallback(response) {
+      console.log('error');
+    });
+  };
+
+  //function for hilarious quotes
+  $scope.laugh = function() {
+    // $scope.quote = "Laughter is the cure all for all ailments";
+    var options = ['laughter', 'hilarious', 'humor', 'tso-funny', 'laugh', 'funny', 'sense-of-humor', 'fun', 'joke', 'geek'];
+    var ranNum = Math.floor(Math.random() * 10);
+    $http({
+      method: 'GET',
+      url: search + options[ranNum] + key
+    }).success(function successCallback(response) {
+      console.log('success');
+      var quote = response.contents.quote;
+      var author = response.contents.author;
+      $scope.quote = quote;
+      $scope.author = author;
+    },
+    function errorCallback(response) {
+      console.log('error');
+    });
+  };
+
+//**************************************************************************************
+
+
+
+
+  $scope.add = function() {
+    if ($scope.quote === undefined) {
+      alert('Nothing to add.');
+    } else {
+      alert('Quote added to your favorites!')
+      $scope.favorites.push($scope.quote + ' -- ' + $scope.author);
+      console.log($scope.favorites)
+    }
+  };
+
+  $scope.show = function() {
+
+  }
+})
+
+
+
+
+
+
+
+
+
+
+//below was test to send get requests with ajax/jquery
+//*************************************************************************************
+// $(document).ready(function(){
+//   var getQuoteCategories = {
+//     type: 'get',
+//     url: 'http://quotes.rest/quote.json?category=inspire&api_key=9lfbA8Lt0DgYlKpNgV4hkweF',
+//     // dataType: 'json',
+//     success: function(data) {
+//         console.log('Pulled from API!!');
+//         console.log(data);
+//                    },
+//            error: function() {
+//                         console.log('Something did not happen as intended');
+//                         alert('something went wrong!!!')
+//                    },
+//       //      beforeSend: function(xhr) {
+//       // xhr.setRequestHeader("X-Theysaidso-Api-Secret","9lfbA8Lt0DgYlKpNgV4hkweF");
+//       // xhr.setRequestHeader('Accept','application/json');
+//       //            }
+//   }
+//   $.ajax(getQuoteCategories);
+// });
+//*************************************************************************************
+
+
+
+
+
---
+++ Inspire/server.js
@@ -0,0 +1,31 @@
+var express        = require('express');
+var app            = express();
+var fs             = require('fs');
+var http           = require('http');
+
+var port = 8080; 
+
+// set the static files location /public/img will be /img for users
+app.use(express.static(__dirname + '/public')); 
+
+
+app.get('/', function(req, res) {
+  fs.readFile('./index.html', function(error, data) {
+    if (error) {
+      res.writeHead(500);
+      res.end();
+    }
+    res.writeHead(200);
+    res.end(data);
+  })
+})
+
+// start app ===============================================
+// startup our app at http://localhost:8080
+app.listen(port);               
+
+// shoutout to the user                     
+console.log('Magic happens on port ' + port);
+
+// expose app           
+exports = module.exports = app;              
