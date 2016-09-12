63b265d280eee02a75f79121ecba7e43c133e5da
Aralya Phinith <aphinith84@gmail.com>
2016-09-11 17:44:34 -0700

    configure server file to send http request to quotes api

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express        = require('express');
 var app            = express();
-var fs             = require('fs');
 var http           = require('http');
+var request        = require('request');
 
 var port = process.env.PORT || 4000; 
 
@@ -11,6 +11,27 @@ app.use(express.static(__dirname + '/public'));
 // start app ===============================================
 // startup our app at http://localhost:4000
 app.listen(port);               
+
+
+/**************************************************************/
+  var search = 'http://quotes.rest/quote.json?category=';
+  var key = '&api_key=cy7IIIwDoKOKCIqNr_jDCQeF';
+  var ranNum = Math.floor(Math.random() * 10);
+
+
+app.get('/inspire', function(req, res) {
+  console.log('got inside server side for inspire quotes');
+    var options = ['inspire', 'positive', 'confidence', 'self-improvement', 'happiness', 'attitude', 'motivational', 'inspirational', 'success', 'successful-mind'];
+  request(search + 'inspire' + key, function(error, response, body) {
+    console.log('this is error:', error);
+    console.log('inside server and this is response:', response)
+  })
+  res.send('ok');
+})
+
+
+/**************************************************************/
+
 
 // shoutout to the user                     
 console.log('Magic happens on port ' + port);
