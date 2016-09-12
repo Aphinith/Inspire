a0994672883ac2ea86b7c8777bc00616bd468b17
Aralya Phinith <aphinith84@gmail.com>
2016-09-11 18:00:58 -0700

    configure quotesServices file to correctly connect with server and controller to send and receive quotes

--- public/services/QuoteServices.js
+++ public/services/QuoteServices.js
@@ -2,18 +2,17 @@ angular.module('QuoteServices',[])
 
 .factory('RQFactory', function($http) {
 
-  // var test = "successful test";
-  var search = 'http:///quotes.rest/quote.json?category=';
-  var key = '&api_key=cy7IIIwDoKOKCIqNr_jDCQeF';
-  var ranNum = Math.floor(Math.random() * 10);
-
   var getInspireQuotes = function() {
     console.log('got inside inspire quotes factory');
-    var options = ['inspire', 'positive', 'confidence', 'self-improvement', 'happiness', 'attitude', 'motivational', 'inspirational', 'success', 'successful-mind'];
+    var results;
     return $http({
       method: 'GET',
-      url: search + options[ranNum] + key,
+      url: '/inspire',
+    }).then(function(res) {
+      results = res;
+      return results
     });
+
   };
 
   var getWisdomQuotes = function() {
